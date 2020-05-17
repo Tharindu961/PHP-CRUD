8cd02c8d1bee7d1ad3a25cd81015103bb437346e
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-17 18:52:50 +0530

    added bootstrap

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<title>PHP CRUD</title>
 
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<title></title>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
+	<div class="row justify-content-center">
+	<form action="" method="POST">
+		<div class="form-group">
+		<label>Name</label>	
+		<input type="text" name="name" class="form-control" value="Enter your name">
+		</div>
+		<div class="form-group">
+		<label>Location</label>
+		<input type="text" name="location" class="form-control" value="Enter your location">
+		</div>
+		<div class="form-group">
+		<button type="submit" class="btn btn-primary" name="save">Save</button>
+		</div>
+	</form>
+	</div>
 
 </body>
 </html>
